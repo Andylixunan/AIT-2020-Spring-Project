f139ed344ce8d9d03b2eb3adf4a3fb6ad61bef11
Andylixunan <xl2216@nyu.edu>
2020-04-14 16:54:14 +0800

    update

--- app.js
+++ app.js
@@ -5,9 +5,12 @@ const Strategy = require('passport-facebook').Strategy;
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-
+const mongoose = require('mongoose');
 const app = express();
-
+const Photo = mongoose.model('Photo');
+const Album = mongoose.model('Album');
+const User = mongoose.model('User');
+/*
 passport.use(new Strategy({
   clientID: process.env['FACEBOOK_CLIENT_ID'],
   clientSecret: process.env['FACEBOOK_CLIENT_SECRET'],
@@ -23,7 +26,7 @@ passport.serializeUser(function(user, cb) {
 passport.deserializeUser(function(obj, cb) {
   cb(null, obj);
 });
-
+*/
 
 // enable sessions
 const session = require('express-session');
@@ -50,12 +53,22 @@ app.get('/', (req, res) => {
 
 app.get('/photo/create', (req, res) => {
   res.render('createPhoto');
-
 });
 
 app.post('/photo/create', (req, res) => {
   
-
+  res.redirect('/album');
 })
+
+app.get('/album',(req, res)=>{
+
+  res.render('album');
+});
+app.get('/album/create',(req, res)=>{
+
+  res.render('createAlbum');
+});
+
+
 
 app.listen(3000);
