ac0e78f2ae55933211d3cae15938e052cce5298a
Andylixunan <xl2216@nyu.edu>
2020-04-22 10:22:19 +0800

    add db configuration

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 require('./db');
 
 const passport = require('passport');
-const Strategy = require('passport-facebook').Strategy;
+const Strategy = require('passport-local').Strategy;
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
@@ -10,31 +10,6 @@ const app = express();
 const Photo = mongoose.model('Photo');
 const Album = mongoose.model('Album');
 const User = mongoose.model('User');
-
-passport.use(new Strategy({
-  clientID: process.env['FACEBOOK_CLIENT_ID'],
-  clientSecret: process.env['FACEBOOK_CLIENT_SECRET'],
-  callbackURL: '/return'
-},
-function(accessToken, refreshToken, profile, cb) {
-  return cb(null, profile);
-}));
-passport.serializeUser(function(user, cb) {
-  cb(null, user);
-});
-
-passport.deserializeUser(function(obj, cb) {
-  cb(null, obj);
-});
-
-// enable sessions
-const session = require('express-session');
-const sessionOptions = {
-    secret: 'secret cookie thang (store this elsewhere!)',
-    resave: true,
-      saveUninitialized: true
-};
-app.use(session(sessionOptions));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -72,6 +47,12 @@ app.get('/album/create',(req, res)=>{
 
   res.render('createAlbum');
 });
+
+app.get('/photo/test', (req, res)=>{
+  Photo.find({}, function(err, data, count){
+    res.send(data);
+  });
+})
 
 
 
