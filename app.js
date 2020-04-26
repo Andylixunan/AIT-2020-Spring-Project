20dbf939e6db5f5bf028024660b8a5f9461b11f1
Andylixunan <xl2216@nyu.edu>
2020-04-27 00:30:52 +0800

    add findone

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const bodyParser = require('body-parser');
-//const urlSlug = require('url-slug');
+const urlSlug = require('url-slug');
 
 const mongoose = require('mongoose');
 const Photo = mongoose.model('Photo');
@@ -30,12 +30,12 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 
-/*
+
 app.use(function(req, res, next){
   res.locals.user = req.user;
 	next();
 });
-*/
+
 
 // route handlers
 app.get('/', (req, res) => {
@@ -55,17 +55,17 @@ app.post('/photo/create', (req, res) => {
     name: req.body.photoName,
     url: req.body.photoUrl
     });
-  photoObj.save(function(err, varToStoreResult, count){
+  photoObj.save(function(err, varToStoreResult){
+    //console.log(varToStoreResult._id);
     res.redirect('/album');
   });
 })
 
 app.get('/album',(req, res)=>{
-  res.render('album', {user: req.user});
+  res.render('album', {user: res.locals.user});
 });
 
 app.get('/album/create',(req, res)=>{
-
   res.render('createAlbum');
 });
 
@@ -74,24 +74,27 @@ app.post('/album/create',(req, res)=>{
     name: req.body.albumName,
     photos: []
   });
-  albumObj.save(function(err, varToStoreResult, count){
+  
+  albumObj.save(function(err, varToStoreResult){
+    User.findOne({username: res.locals.user.username}, function(err, data){
+      //console.log(data._id);
+    });
     res.redirect('/album');
   });
 });
 
 // add test site for viewing and testing all the working forms
 app.get('/photo/test', (req, res)=>{
-  Photo.find({}, function(err, data, count){
+  Photo.find({}, function(err, data){
     res.send(data);
   });
 })
 
 app.get('/album/test', (req, res)=>{
-  Album.find({}, function(err, data, count){
+  Album.find({}, function(err, data){
     res.send(data);
   });
 })
-
 
 
 app.get('/register', (req, res) =>{
@@ -104,7 +107,6 @@ app.post('/register', (req, res) =>{
         console.log(err);
         res.render('register');
       }
-      
       const authenticate = User.authenticate();
       authenticate(req.body.registerUsername, req.body.registerPassword, function(err,){
         if(err){
@@ -112,8 +114,6 @@ app.post('/register', (req, res) =>{
         }
         res.redirect('/')
       })
-      
-
   });
 });
 
