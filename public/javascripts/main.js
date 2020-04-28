7fe0bb52fcf1ea4fb93d45cdb06c40ef14654166
Andylixunan <xl2216@nyu.edu>
2020-04-29 03:45:45 +0800

    add client side form validation

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,1 +1,29 @@
 // this javascript is for client-side form validation
+const createAlbumForm = document.querySelector('#createAlbumForm');
+const createAlbumInput = document.querySelector('#createAlbumInput');
+const createAlbumInputErr = document.querySelector('#createAlbumError');
+const createPhotoForm = document.querySelector('#createPhotoForm');
+const createPhotoNameInput = document.querySelector('#createPhotoNameInput');
+const createPhotoUrlInput = document.querySelector('#createPhotoUrlInput');
+const createPhotoError = document.querySelector('#createPhotoError');
+
+createAlbumForm.addEventListener('submit', function (event) {
+    if (createAlbumInput.value.length == 0) {
+        createAlbumInputErr.textContent = 'You need to enter an album name. (This field cannot be empty)';
+        createAlbumInputErr.className = 'error active';
+        event.preventDefault();
+    }
+    else if (createAlbumInput.value.length >= 30) {
+        createAlbumInputErr.textContent = 'You need to shorten your album name';
+        createAlbumInputErr.className = 'error active';
+        event.preventDefault();
+    }
+});
+
+createPhotoForm.addEventListener('submit', function (event) {
+    if (createPhotoNameInput.value.length == 0 || createPhotoUrlInput.value.length == 0) {
+        createPhotoError.textContent = 'You need to enter a photo name and a url. (Both fields cannot be empty)';
+        createPhotoError.className = 'error active';
+        event.preventDefault();
+    }
+});
